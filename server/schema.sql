3f31d84fc3c500f6cca8d085537f041fea557609
YorkeF <yorke.ferrell@okstate.edu>
2026-03-31 14:07:55 -0500

    small sql version fixes

--- server/schema.sql
+++ server/schema.sql
@@ -36,7 +36,8 @@ CREATE TABLE IF NOT EXISTS lizard_weights (
   UNIQUE KEY uq_lizard_date (lizard_id, weighed_on)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TRIGGER IF NOT EXISTS trg_weights_after_insert
+DROP TRIGGER IF EXISTS trg_weights_after_insert;
+CREATE TRIGGER trg_weights_after_insert
 AFTER INSERT ON lizard_weights
 FOR EACH ROW
   UPDATE lizards
@@ -47,7 +48,8 @@ FOR EACH ROW
   )
   WHERE id = NEW.lizard_id;
 
-CREATE TRIGGER IF NOT EXISTS trg_weights_after_delete
+DROP TRIGGER IF EXISTS trg_weights_after_delete;
+CREATE TRIGGER trg_weights_after_delete
 AFTER DELETE ON lizard_weights
 FOR EACH ROW
   UPDATE lizards
